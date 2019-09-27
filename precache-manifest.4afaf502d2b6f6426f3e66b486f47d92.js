90f1e774fdefd0acfe1691edf560dcb865b0687c
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-27 15:16:00 -0500

    Updates

---
+++ precache-manifest.4afaf502d2b6f6426f3e66b486f47d92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e57f5ef691b969efe56fe2277a2660",
+    "url": "/unit4assessment/index.html"
+  },
+  {
+    "revision": "6abbb074dfac25dec881",
+    "url": "/unit4assessment/static/css/main.c04c135c.chunk.css"
+  },
+  {
+    "revision": "9d1986347ffb90192213",
+    "url": "/unit4assessment/static/js/2.fbaef442.chunk.js"
+  },
+  {
+    "revision": "6abbb074dfac25dec881",
+    "url": "/unit4assessment/static/js/main.3023196c.chunk.js"
+  },
+  {
+    "revision": "d6d11fc09083221b2800",
+    "url": "/unit4assessment/static/js/runtime-main.970421bd.js"
+  }
+]);
